d71e65618d754b90eb3c3dc46dc4c50bc5e8a5ff
user_2 <user_2@DDU-44>
2019-09-17 12:50:06 +0530

    calccc

--- src/main/java/main/Calculator.java
+++ src/main/java/main/Calculator.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Calculator {
     public static void main(String[] args)
     {
-        System.out.println("Enter first and second number:");
+        System.out.println("Enterfirst and second number:");
         Scanner inp= new Scanner(System.in);
         int num1,num2;
         num1 = inp.nextInt();
